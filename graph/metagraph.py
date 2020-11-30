b46b34eebc8843561c8da865461a5edd905a165c
Spencer R Loggia <spencer.loggia98@gmail.com>
2020-11-30 11:42:41 -0500

    add get adjacency method, fix load bug

--- graph/metagraph.py
+++ graph/metagraph.py
@@ -25,14 +25,16 @@ def _get_att_from_json(json_path: str, evaluate_keys=False) -> dict:
 
 class Metagraph:
     """
-    single graph only so far
+    Wrapper for networkx `Graph` or `DiGraph` object. Provides methods for writing to / loading from file.
+    Limitations - networkx graph's nodes must be either int or string. Multigraphs are represented as lists of
+    graphs.
     """
 
     def __init__(
-        self,
-        graphs: List[Union[nx.Graph, nx.DiGraph]] = None,
-        edge_att_names: List[set] = None,
-        node_att_names: List[set] = None,
+            self,
+            graphs: List[Union[nx.Graph, nx.DiGraph]] = None,
+            edge_att_names: List[set] = None,
+            node_att_names: List[set] = None,
     ):
         # nodes must be basic data types
         if graphs is None:
@@ -42,6 +44,7 @@ class Metagraph:
         else:
             self.nx_obj = graphs
             if edge_att_names is None:
+                # find edge attributes names if not provided.
                 self.edge_att_names = []
                 for graph in graphs:
                     self.edge_att_names.append(
@@ -77,7 +80,42 @@ class Metagraph:
         self.edge_att_names = []
         self.node_att_names = []
 
+    def get_adjacency(self, return_node_attributes=True, return_edge_attributes=True) -> \
+            List[np.ndarray] or List[np.ndarray, dict] or List[np.ndarray, dict, dict]:
+        """
+        gets the dense representation of all graphs' adjacency matrices as list of numpy arrays, and provides lists of
+        node / edge attributes mapped to indexes in adjacency matrix.
+        :return: np.ndarray
+        """
+        adj_mats = []
+        edge_atts = []
+        node_atts = []
+        for i in range(len(self.nx_obj)):
+            graph = self.nx_obj[i]
+            conv = nx.convert_node_labels_to_integers(graph, ordering="sorted")
+            adj_mats.append(nx.to_numpy_array(conv, dtype=np.int32))
+            if return_edge_attributes:
+                edge_atts.append({})
+                for eatt in self.edge_att_names[i]:
+                    edge_atts[-1][eatt] = nx.get_edge_attributes(conv, eatt)
+            if return_node_attributes:
+                node_atts.append({})
+                for natt in self.node_att_names[i]:
+                    node_atts[-1][natt] = nx.get_node_attributes(conv, natt)
+        to_return = [adj_mats]
+        if return_node_attributes:
+            to_return.append(node_atts)
+        if return_edge_attributes:
+            to_return.append(edge_atts)
+        return to_return
+
     def dump(self, path: str):
+        """
+        Write graph to disk at specified path.
+        :param path: path to write to, without extension
+        :return: None
+        """
+
         uuid = "{:4}".format(np.random.randint(0, 9999))
         tmpdir = "./tmp" + uuid
         os.mkdir(tmpdir)
@@ -105,6 +143,10 @@ class Metagraph:
         shutil.rmtree(tmpdir)
 
     def load(self, path: str):
+        """
+        load metagraph from disk.
+        :param path: location of graph file.
+        """
         uuid = "{:4}".format(np.random.randint(0, 9999))
         tmpdir = "./tmp" + uuid
         os.mkdir(tmpdir)
@@ -130,7 +172,7 @@ class Metagraph:
                         e_att_names.append(attname)
                     elif "nodeatt" in file:
                         att = _get_att_from_json(
-                            os.path.join(tmpdir, graph_dir, file), evaluate_keys=False
+                            os.path.join(tmpdir, graph_dir, file), evaluate_keys=True
                         )
                         nx.set_node_attributes(g, att, attname)
                         n_att_names.append(attname)
